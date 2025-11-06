3cc8a534ea48bd0c2f2e9afee805ddc6ea84b8ea
zahoor <zahoor.uk.edu@gmail.com>
2025-11-06 15:45:20 +0000

    Commit for task 4. Used the style property To change the fontsize, fontweight and color of the text.

--- lib/main.dart
+++ lib/main.dart
@@ -46,7 +46,13 @@ class OrderItemDisplay extends StatelessWidget {
 
 @override
 Widget build(BuildContext context) {
-  return Text('$quantity $itemType sandwich(es): ${'🥪' * quantity}');
+  return Text('$quantity $itemType sandwich(es): ${'🥪' * quantity}',
+  style: const TextStyle(
+    color: Colors.green,
+    fontWeight:FontWeight.bold,
+    fontSize: 20,
+  ),
+);
 }
 }
 
