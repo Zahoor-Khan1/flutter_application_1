4cdce717dc3fd1c816eb7a1d560384aa03041e28
zahoor <zahoor.uk.edu@gmail.com>
2025-12-11 15:43:48 +0000

    Create ProfileScreen StatefulWidget structure

---
+++ lib/views/profile_screen.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:sandwich_shop/views/app_styles.dart';
+
+class ProfileScreen extends StatefulWidget {
+  const ProfileScreen({super.key});
+
+  @override
+  State<ProfileScreen> createState() => _ProfileScreenState();
+}
