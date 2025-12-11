c63a5e36ac2e174e9e8c54a9e7b54957d4acff32
zahoor <zahoor.uk.edu@gmail.com>
2025-12-11 15:06:05 +0000

    Refactor CartScreen to StatefulWidget

--- lib/views/cart_screen.dart
+++ lib/views/cart_screen.dart
@@ -9,7 +9,13 @@ class CartScreen extends StatefulWidget {
   const CartScreen({super.key, required this.cart});
 
   @override
+  State<CartScreen> createState() => _CartScreenState();
+}
+
+class _CartScreenState extends State<CartScreen> {
+  @override
   Widget build(BuildContext context) {
+
     return Scaffold(
       appBar: AppBar(
         title: const Text("Cart View", style: heading1),
