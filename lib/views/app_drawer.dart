ac275b1620c1c6abbaa2dd0548e3b070a4a2ff18
zahoor <zahoor.uk.edu@gmail.com>
2025-12-12 11:35:06 +0000

    Add DrawerHeader to AppDrawer

--- lib/views/app_drawer.dart
+++ lib/views/app_drawer.dart
@@ -8,3 +8,25 @@ class AppDrawer extends StatelessWidget {
     return const Drawer();
   }
 }
+@override
+Widget build(BuildContext context) {
+  return Drawer(
+    
+    child: ListView(
+      children: const [
+        DrawerHeader(
+          decoration: BoxDecoration(color: Colors.green),
+          child: Text(
+            'Sandwich Shop',
+            style: TextStyle(
+              color: Colors.white,
+              
+              fontSize: 24,
+            ),
+          ),
+        ),
+      ],
+    ),
+  );
+  
+}
