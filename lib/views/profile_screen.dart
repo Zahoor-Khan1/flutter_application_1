8f9003fdb52342ba8122bf9e91d3d5bf28a1ce82
zahoor <zahoor.uk.edu@gmail.com>
2025-12-12 13:24:46 +0000

    removed the unused library

--- lib/views/profile_screen.dart
+++ lib/views/profile_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:sandwich_shop/views/app_styles.dart';
 import 'package:sandwich_shop/views/app_drawer.dart';
 import 'package:sandwich_shop/views/common_widgets.dart';
 
