a05c9158745f90a214509c7043ab042edd5466c6
zahoor <zahoor.uk.edu@gmail.com>
2025-12-12 13:19:58 +0000

    Replace ProfileScreen AppBar with common AppBar

--- lib/views/profile_screen.dart
+++ lib/views/profile_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:sandwich_shop/views/app_styles.dart';
 import 'package:sandwich_shop/views/app_drawer.dart';
+import 'package:sandwich_shop/views/common_widgets.dart';
 
 class ProfileScreen extends StatefulWidget {
   const ProfileScreen({super.key});
@@ -16,9 +17,8 @@ class _ProfileScreenState extends State<ProfileScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Profile", style: heading1),
-      ),
+appBar: buildCommonAppBar(title: 'Profile'),
+
       drawer: const AppDrawer(),
 
       body: Padding(
