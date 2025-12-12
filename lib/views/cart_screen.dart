3021fa5bffa5e7850b804d15eaacd33266f2ea8d
zahoor <zahoor.uk.edu@gmail.com>
2025-12-12 11:46:41 +0000

    Attach navigation drawer to CartScreen

--- lib/views/cart_screen.dart
+++ lib/views/cart_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:sandwich_shop/models/cart.dart';
 import 'package:sandwich_shop/views/app_styles.dart';
 import 'package:sandwich_shop/views/checkout_screen.dart';
+import 'package:sandwich_shop/views/app_drawer.dart';
 
 class CartScreen extends StatefulWidget {
   final Cart cart;
@@ -58,6 +59,8 @@ class _CartScreenState extends State<CartScreen> {
       appBar: AppBar(
         title: const Text("Cart View", style: heading1),
       ),
+      drawer: const AppDrawer(),
+
       body: Padding(
         padding: const EdgeInsets.all(20.0),
         child: Column(
