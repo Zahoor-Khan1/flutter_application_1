0a0dbfc888e3e644724a11482ee9d95c0b911c29
zahoor <zahoor.uk.edu@gmail.com>
2025-12-12 13:15:10 +0000

    Enhance common AppBar to support optional logo

--- lib/views/common_widgets.dart
+++ lib/views/common_widgets.dart
@@ -3,8 +3,18 @@ import 'package:sandwich_shop/views/app_styles.dart';
 
 PreferredSizeWidget buildCommonAppBar({
   required String title,
+bool showLogo = false,
 }) {
   return AppBar(
+    leading: showLogo
+        ? Padding(
+            padding: const EdgeInsets.all(6.0),
+            child: Image.asset(
+              'assets/images/logo.png',
+              fit: BoxFit.contain,
+            ),
+          )
+        : null,
     title: Text(
       title,
       style: heading1,
