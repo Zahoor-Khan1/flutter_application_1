7a1cd788c66a6170f45a0c282cfa0131ffc18706
zahoor <zahoor.uk.edu@gmail.com>
2025-12-19 02:05:11 +0000

    Add integration_test dependency for integration testing

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 import 'package:sandwich_shop/main.dart';
